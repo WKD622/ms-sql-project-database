2daa0b23b18af00a3a51b01dffdbc92114367a12
Kamil Jarosz <kamil09875@gmail.com>
2018-01-14 14:29:27 +0100

    add views

--- schema/06-views.sql
+++ schema/06-views.sql
@@ -6,10 +6,75 @@
  */
 
 /**
+ * Widok podsumowujący klientów.
+ * 
+ * @tested
+ */
+create view CustomersSummary as
+	select
+			c.CustomerID,
+			(cast(case
+				when comp.CustomerID is not null then 1
+				else 0
+			end as bit))
+				as IsCompany,
+			(case
+				when comp.CustomerID is not null then comp.CompanyName
+				else p.FirstName + ' ' + p.LastName
+			end)
+				as Name,
+			comp.NIP,
+			c.Address,
+			c.Phone,
+			c.Email,
+			c.Login
+		from Customers as c
+			left join Companies as comp
+				on comp.CustomerID = c.CustomerID
+			left join Persons as per
+				on per.CustomerID = c.CustomerID
+			left join Participants as p
+				on p.ParticipantID = per.ParticipantID;
+go
+
+/**
+ * Widok aktywności klientów.
+ */
+create view ActiveCustomers as
+	select
+			cs.CustomerID,
+			cs.IsCompany,
+			cs.Name,
+			(count(distinct DayBookingID))
+				as DayBookingsCount,
+			(count(distinct WorkshopBookinngID))
+				as WorkshopBookingsCount,
+			(count(distinct DayBookingID) + count(distinct WorkshopBookinngID))
+				as BookingsCount
+		from CustomersSummary as cs
+			left join Bookings as b
+				on b.CustomerID = cs.CustomerID
+			left join DayBookings as db
+				on db.BookingID = b.BookingID
+			left join WorkshopBookings as wb
+				on wb.DayBookingID = db.DayBookingID
+		group by cs.CustomerID, cs.IsCompany, cs.Name;
+go
+
+/**
  * Widok rezerwacji, które nie zostały jeszcze zapłacone
  * oraz ich termin płatności się zakończył.
+ * 
+ * @column Delay
+ *     ilość dni spóźnienia
  */
 create view UnpaidBookings as
-	select * from Bookings
+	select
+			BookingID,
+			BookingDate,
+			DueDate,
+			PaymentDate,
+			(datediff(day, getdate(), DueDate)) as Delay
+		from Bookings
 		where Paid = 0 and getdate() > DueDate;
 go
