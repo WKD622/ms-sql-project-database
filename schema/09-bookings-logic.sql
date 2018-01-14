e5e82bfec2da9f10c7edb3c7f1887502ce9fcea1
Jakub Ziarko <kuba.ziarko@gmail.com>
2018-01-14 19:32:45 +0100

    add setPaid

--- schema/09-bookings-logic.sql
+++ schema/09-bookings-logic.sql
@@ -1,1 +1,25 @@
-test
+/**
+ * Ustawia datę wykonania płatności dla wybranego BookingID 
+ */
+create procedure setPaid (
+	@bookingID    int,
+	@paymentDate  date,
+	@todaysDate   date = null
+) as 
+	set xact_abort on;
+	begin transaction;
+		if @todaysDate is not null
+		begin 
+			update Bookings
+			set PaymentDate = @paymentDate
+			where BookingID = @bookingID;
+		end
+		if @todaysDate is null
+		begin
+			update Bookings
+			set PaymentDate = GETDATE() 
+			where BookingID = @bookingID;
+		end
+	commit transaction
+go
+
