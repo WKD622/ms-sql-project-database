55155343573f5926ef0283bbd8622cdac0e5381f
Jakub Ziarko <kuba.ziarko@gmail.com>
2018-01-14 20:59:24 +0100

    add procedures booking & dayBooking & Workshopbooking

--- schema/09-bookings-logic.sql
+++ schema/09-bookings-logic.sql
@@ -17,9 +17,70 @@ create procedure setPaid (
 		if @todaysDate is null
 		begin
 			update Bookings
-			set PaymentDate = GETDATE() 
+			set PaymentDate = getdate() 
 			where BookingID = @bookingID;
 		end
 	commit transaction
 go
 
+/**
+ * Dodaje Booking
+ */
+create procedure addBookings (
+	@customerID      int
+) as 
+	set xact_abort on;
+	begin transaction
+		declare @bookingDate date = getdate();
+		declare @dueDate date = dateadd(day, 7, @bookingDate);
+		declare @paymentDate date = null;
+		
+		insert into DayBookings (
+			CustomerID, BookingDate,
+			DueDate, PaymentDate
+		) values (
+			@customerID, @bookingDate,
+			@dueDate, @paymentDate
+		);
+	commit transaction
+go
+
+/**
+ * Dodaje dayBooking
+ */
+create procedure addDayBooking (
+	@bookingID       int,
+	@conferencedayID int,
+	@participants    int
+) as 
+	set xact_abort on;
+	begin transaction
+		insert into DayBookings (
+			BookingID, ConferenceDayID,
+			Participants
+		) values (
+			@bookingID, @conferencedayID,
+			@participants
+		);
+	commit transaction
+go
+
+/**
+ * Dodaje WorkshopBooking 
+ */ 
+ create procedure addWorkshopBooking(
+	@workshopTermID    int,
+	@dayBookingID      int,
+	@participants      int
+) as 
+	set xact_abort on;
+	begin transaction
+		insert into WorkshopBookings (
+			WorkshopTermID, DayBookingID,
+			Participants
+		) values (
+			@workshopTermID, @dayBookingID,
+			@participants
+		);
+	commit transaction
+go
