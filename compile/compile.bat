b59ab1ad8855d05c830670cc4ee5b15ed5b402da
Kamil Jarosz <kamil09875@gmail.com>
2018-01-16 13:46:57 +0100

    add tests compiling

--- compile/compile.bat
+++ compile/compile.bat
@@ -9,3 +9,15 @@ echo /* This file is generated >> %out%
 date /t >> %out%
 time /t >> %out%
 echo */ >> %out%
+
+:::::::::::::::::::::::::::::::::::::::::::::::
+
+cd ../test
+
+set out="../out/tests.sql"
+copy /b *.sql %out%
+
+echo /* This file is generated >> %out%
+date /t >> %out%
+time /t >> %out%
+echo */ >> %out%
